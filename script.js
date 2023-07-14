92b2f71bd044d36ace806c3f16571f2b51b2d5ae
Thybow <tmilville.pro@gmail.com>
2023-07-14 09:20:49 +0200

    initialization for the image size

--- script.js
+++ script.js
@@ -1,14 +1,21 @@
 class ImageContainerSize {
   constructor() {
+    // Variables
     this.mainImage = document.getElementById('main-image');
     this.imageContainer = document.querySelector('.image');
 
+    // Bindings
     this.adjustImageContainerSize = this.adjustImageContainerSize.bind(this);
 
+    // Events
     this.mainImage.addEventListener('load', this.adjustImageContainerSize);
     window.addEventListener('resize', this.adjustImageContainerSize);
+
+    // Initializations
+    this.adjustImageContainerSize();
   }
 
+  // Function to adjust the size of the image container
   adjustImageContainerSize() {
     const width = this.mainImage.offsetWidth;
     const height = this.mainImage.offsetHeight;
